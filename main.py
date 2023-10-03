66dcaa3571bcd93adc0c5ef65f0eb82b71cdd5de
jet <561009@bk.ru>
2023-10-03 12:33:12 +0300

    Юнит-тест

--- main.py
+++ main.py
@@ -1,40 +1,61 @@
 import json
-import sys
-import os
 import re
+import os
+import sys
+import unittest
 
 
-def process_data(data_file, replacement_file, output_file):
+class DataProcessor:
+    def __init__(self, data_file, replacement_file, output_file):
+        self.data_file = data_file
+        self.replacement_file = replacement_file
+        self.output_file = output_file
 
-    with open(replacement_file) as f:
-        replacements = json.load(f)
+    def __load_data(self, file_path):
+        with open(file_path) as f:
+            return json.load(f)
+    def __save_data(self, data):
+        with open(self.output_file, "w") as f:
+            json.dump(data, f, ensure_ascii=False)
 
-    with open(data_file) as f:
-        data = json.load(f)
 
-    filtered_replacements = [replacement for replacement in replacements if replacement["source"] is not None]
-    new_replacements = {}
+    def __clean_replacements(self, replacements):
+        filtered_replacements = [r for r in replacements if r["source"] is not None]
+        unique_replacements = {}
 
-    for replacement in filtered_replacements:
-        if replacement["replacement"] in new_replacements:
-            filtered_replacements.remove(replacement)
-        else:
-            new_replacements[replacement["replacement"]] = replacement["source"]
+        for replacement in filtered_replacements:
+            if replacement["replacement"] not in unique_replacements:
+                unique_replacements[replacement["replacement"]] = replacement["source"]
 
-    sorted_replacements = {k: v for k, v in sorted(new_replacements.items(), key=lambda item: len(item[1]), reverse=True)}
+        return {k: v for k, v in sorted(unique_replacements.items(), key=lambda item: len(item[1]), reverse=True)}
 
-    new_data = data.copy()
+    def __replace_data(self, data, replacements):
+        new_data = data.copy()
 
-    for rep_key, rep_value in sorted_replacements.items():
-        regex = re.compile(re.escape(rep_key))
-        new_data = [regex.sub(rep_value, message) for message in new_data]
+        for rep_key, rep_value in replacements.items():
+            regex = re.compile(re.escape(rep_key))
+            new_data = [regex.sub(rep_value, message) for message in new_data]
 
-    new_data = [new_message for orig_message, new_message in zip(data, new_data) if orig_message != new_message]
-    print(new_data)
+        # return new_data
 
-    with open(output_file, "w") as f:
-        json.dump(data, f, ensure_ascii=False)
+        return [new_message for orig_message, new_message in zip(data, new_data) if orig_message != new_message]
 
+    def process_data(self):
+        # Загружаем данные замен из файла replacement_file
+        replacements = self.__load_data(self.replacement_file)
+
+        # Загружаем исходные данные из файла data_file
+        data = self.__load_data(self.data_file)
+
+        # Очищаем набор замен, удаляя дубликаты и значения с пустыми источниками,
+        # а также сортируем замены в порядке убывания их длины
+        new_replacements = self.__clean_replacements(replacements)
+
+        # Производим замену исходного текста в data, используя очищенные замены new_replacements
+        new_data = self.__replace_data(data, new_replacements)
+
+        # Сохраняем измененные данные в файл output_file
+        self.__save_data(new_data)
 
 def get_file_path(prompt):
     while True:
@@ -50,14 +71,22 @@ def get_file_path(prompt):
             print("\nВыход из программы")
             sys.exit()
 
+def program_start(data_file, replacement_file, output_file):
 
-def program_start():
-    data_file = get_file_path("Пожалуйста, введите путь к файлу data.json: ")
-    replacement_file = get_file_path("Пожалуйста, введите путь к файлу replacement.json: ")
-    output_file = input("Пожалуйста, введите имя и/или путь для файла вывода (result.json): ")
+    # data_file = get_file_path("Пожалуйста, введите путь к файлу data.json: ")
+    #
+    # replacement_file = get_file_path("Пожалуйста, введите путь к файлу replacement.json: ")
+    #
+    # output_file = input("Пожалуйста, введите имя и/или путь для файла вывода (result.json): ")
 
-    process_data("data.json", "replacement.json", "re1.json")
-    # print("Обработка данных завершена. Результат сохранен в файле:", output_file)
+    processor = DataProcessor(data_file, replacement_file, output_file)
+
+    processor.process_data()
+
+    print("Обработка данных завершена. Результат сохранен в файле:", output_file)
+
+
+
 
 
 if __name__ == "__main__":
