ce5bd84dd4017114d3f5c3b74ce27dcb8af98809
Utlyakov Andrey <85285612+jettooss@users.noreply.github.com>
2023-10-07 11:41:29 +1000

    fix

--- test.py
+++ test.py
@@ -4,19 +4,6 @@ import unittest
 import json
 
 from main import program_start
-<<<<<<< Updated upstream
-
-
-class TestProcessDataNew(unittest.TestCase):
-
-    # Первый тестовый случай: проверка поведения функции program_start с определёнными аргументами
-    def test_process_data_thewhite(self):
-        # Вызываем функцию program_start с определёнными аргументами и сохраняем результат
-        actual_output=program_start('data.json', 'replacement.json', 'output1.json')
-        # Загружаем ожидаемые данные из файла
-        expected_output = self.load_data_from_file('initial-data.json')
-        # Используем assertEqual для проверки равенства ожидаемого и полученного результатов
-=======
 from main import DataProcessor
 
 class TestProcessDataNew(unittest.TestCase):
@@ -30,13 +17,6 @@ class TestProcessDataNew(unittest.TestCase):
             {"replacement": "test2", "source": None},
             {"replacement": "test1", "source": "src3"}
         ]
-
-    def test_load_data(self):
-        # Загрузка данных
-        data = self.instance._load_data(  self.test_file )
-        # Проверка результата
-        self.assertEqual(data, {"test": "data"})
-
     def test_save_data(self):
         # Сохранения данных
         self.instance.output_file = self.test_file  # Предполагается, что у DataProcessor есть атрибут output_file
@@ -45,8 +25,14 @@ class TestProcessDataNew(unittest.TestCase):
         # Чтение файла и проверка на то что данные те же
         with open(self.test_file, 'r') as f:
             loaded_data = json.load(f)
-
         self.assertEqual(loaded_data, self.test_data)  # Проверка результата
+
+    def test_load_data(self):
+        # Загрузка данных
+        data = self.instance._load_data(  self.test_file )
+        # Проверка результата
+        self.assertEqual(data, {"test": "data"})
+
 
     def test_clean_replacements(self):
         # Очистка замен
@@ -79,21 +65,9 @@ class TestProcessDataNew(unittest.TestCase):
         actual_output = program_start('data.json', 'replacement.json', 'output1.json')
         expected_output = self.load_data_from_file('initial-data.json')
         # Проверка результата
->>>>>>> Stashed changes
         self.assertEqual(actual_output, expected_output)
 
-    # Второй тестовый случай: проверка поведения функции program_start с другими аргументами
     def test_process_data_new(self):
-<<<<<<< Updated upstream
-        # Вызываем функцию program_start с определёнными аргументами и сохраняем результат
-        actual_output=program_start('my_data.json', 'replace.json', 'output2.json')
-        # Загружаем ожидаемые данные из другого файла
-        expected_output = self.load_data_from_file('expected_output_data.json')
-        # Еще раз сравниваем ожидаемый и полученный результаты
-        self.assertEqual(actual_output, expected_output)
-
-    # Функция для загрузки данных из файла: она будет использоваться, чтобы загрузить ожидаемые результаты
-=======
         # Новый тест обработки данных
         actual_output = program_start('my_data.json', 'replace.json', 'output2.json')
         expected_output = self.load_data_from_file('expected_output_data.json')
@@ -101,16 +75,13 @@ class TestProcessDataNew(unittest.TestCase):
         self.assertEqual(actual_output, expected_output)
 
     # Загружаем данные из файла
->>>>>>> Stashed changes
     def load_data_from_file(self, filename):
         with open(filename, 'r') as f:
             data = json.load(f)
         return data
 
-<<<<<<< Updated upstream
-# Если этот скрипт запущен как основной файл, запускаем все тесты
-=======
 
->>>>>>> Stashed changes
 if __name__ == "__main__":
     unittest.main()
+
+
